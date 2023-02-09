edd265df794f64f592888d724759e28fd8c765f4
Adam Naoui <adam.naoui@polymtl.ca>
2023-02-09 11:19:48 -0500

    Q2: better oracle (15 qubits to 10 qubits)

--- TP2/oracle.py
+++ TP2/oracle.py
@@ -4,46 +4,54 @@ from qiskit_aer import QasmSimulator
 from qiskit.visualization import plot_histogram
 import qiskit.quantum_info as qi
 
-target = '01101'
+target = '01101'  # len(target) must be >= 2
 # Use Aer's qasm_simulator
 simulator = QasmSimulator()
 # Create quantum program that find 01101 by reversing its phase
 x = QuantumRegister(len(target), name='X')  # 5 qubits index 0 is the right most qubit
 y = QuantumRegister(1, name='Y')
-ands_results = QuantumRegister(9, name='ands_results')
+ands_results = QuantumRegister(len(target) - 1, name='ands_results')
 res = ClassicalRegister(1, name='res')
 
 oracle = QuantumCircuit(x, y, ands_results, res, name='oracle')
 
 oracle.x(y, 'init y with |1>')
+
 # Apply Hadamard gate to X and Y
 oracle.h(x)
 oracle.h(y)
 
-# Ensure each bit is correct in ands_results
-for i in range(len(target)):
+for i in range(len(target) - 1):
     correct_index = len(target) - i - 1
-    if target[i] == '0':
-        oracle.x(x[correct_index], 'set to |1> for future CX gate')
-        oracle.cx(x[correct_index], ands_results[correct_index])
-        oracle.x(x[correct_index], 'undo')  # Reset x[i]
-    else:
-        oracle.cx(x[correct_index], ands_results[correct_index])
+    if i == 0:
+        if target[correct_index] == '0':
+            oracle.x(x[i], 'correct bit is 0, set to |1> for future CCX gate')
+        if target[correct_index - 1] == '0':
+            oracle.x(x[i + 1],'correct bit is 0, set to |1> for future CCX gate')
 
-# ands_results from 0 to 4 are all to |1> if x is 01101
-# We now have to apply ANDS gates to ands_results
+        oracle.ccx(x[i], x[i + 1], ands_results[i])
 
-oracle.ccx(ands_results[0], ands_results[1], ands_results[5])
-oracle.ccx(ands_results[2], ands_results[3], ands_results[6])
-oracle.ccx(ands_results[5], ands_results[6], ands_results[7])  # ands_results[7] will be at |1> if x is 01101 or 01101
+        if target[correct_index] == '0':
+            oracle.x(x[i], 'undo setter')
+        if target[correct_index - 1] == '0':
+            oracle.x(x[i + 1], 'undo setter')
 
-oracle.ccx(ands_results[7], ands_results[4], ands_results[8])  # ands_results[8] will be at |1> if x is 01101
+        continue
 
-# ands_results[8] contains f(x) value
-oracle.cx(ands_results[8], y)  # Apply CX gate to qubit ands_results[8], y in order to get f(x) output (classical XOR)
+    if target[correct_index - 1] == '0':
+        oracle.x(x[i + 1], 'correct bit is 0, set to |1> for future CCX gate')
+
+    oracle.ccx(ands_results[i - 1], x[i + 1], ands_results[i])
+
+    if target[correct_index - 1] == '0':
+        oracle.x(x[i + 1], 'undo setter')
+
+# ands_results[len(target) - 2] contains f(x) value
+# Apply CX gate to qubit ands_results[8], y in order to get f(x) output (classical XOR)
+oracle.cx(ands_results[len(target) - 2], y)
 
 # Map the quantum measurement to the classical bits
-oracle.measure(ands_results[8], res)
+oracle.measure(ands_results[len(target) - 2], res)
 
 compiled_circuit = transpile(oracle, simulator)
 
